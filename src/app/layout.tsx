5067990a8f4452e91a616c2c15ca73dcdc9e0244
ᴛᴏɴʏ <phollawat@anymindgroup.com>
2023-07-03 22:42:44 +0700

    Update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
-import './globals.css'
-import { Encode_Sans } from 'next/font/google'
+import './globals.css';
+import { Encode_Sans } from 'next/font/google';
 
-const encode = Encode_Sans({ subsets: ['latin'] })
+const encode = Encode_Sans({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Pokemon Table',
+  title: 'Pokemon List',
   description: 'Created for Bright Dolphins',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={encode.className}>{children}</body>
     </html>
-  )
+  );
 }
